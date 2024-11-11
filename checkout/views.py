3430af9d0b38af1c9d83c218d5725cdbd6d4850c
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-11-11 22:35:02 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QK5u4P3kg08sHJNLPBnUrtXYMgGQKxZ9OpjNORwWUxx8yBr6xcBxkvLwnDiRzMa6ZJziX83LoyqsxNotDeQ4ap800hdPKVAYa',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
